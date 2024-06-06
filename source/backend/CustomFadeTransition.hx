9e2ea9adde686c2d6bbaa5ec1b0eeaea55deffb8
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-06-06 17:32:25 -0300

    CustomFadeTransition.hx

--- source/backend/CustomFadeTransition.hx
+++ source/backend/CustomFadeTransition.hx
@@ -19,16 +19,10 @@ class CustomFadeTransition extends FlxSubState {
 	var duration:Float;
 	
 	var loadLeft:FlxSprite;
-	var loadRight:FlxSprite;
 	var loadAlpha:FlxSprite;
-	var WaterMark:FlxText;
-	var EventText:FlxText;
 	
 	var loadLeftTween:FlxTween;
-	var loadRightTween:FlxTween;
 	var loadAlphaTween:FlxTween;
-	var EventTextTween:FlxTween;
-	var loadTextTween:FlxTween;
     
     
 	public function new(duration:Float, isTransIn:Bool) {		
@@ -43,13 +37,6 @@ class CustomFadeTransition extends FlxSubState {
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length-1]];
 		
 		if(ClientPrefs.data.CustomFade == 'Move'){
-		loadRight = new FlxSprite(isTransIn ? 0 : 1280, 0).loadGraphic(Paths.image('menuExtend/CustomFadeTransition/loadingR'));
-		loadRight.scrollFactor.set();
-		loadRight.antialiasing = ClientPrefs.data.antialiasing;		
-		add(loadRight);
-		loadRight.setGraphicSize(FlxG.width, FlxG.height);
-		loadRight.updateHitbox();
-		
 		loadLeft = new FlxSprite(isTransIn ? 0 : -1280, 0).loadGraphic(Paths.image('menuExtend/CustomFadeTransition/loadingL'));
 		loadLeft.scrollFactor.set();
 		loadLeft.antialiasing = ClientPrefs.data.antialiasing;
@@ -57,86 +44,19 @@ class CustomFadeTransition extends FlxSubState {
 		loadLeft.setGraphicSize(FlxG.width, FlxG.height);
 		loadLeft.updateHitbox();
 		
-		WaterMark = new FlxText(isTransIn ? 50 : -1230, 720 - 50 - 50 * 2, 0, 'NF ENGINE V' + MainMenuState.novaFlareEngineVersion, 50);
-		WaterMark.scrollFactor.set();
-		WaterMark.setFormat(Assets.getFont("assets/fonts/loadText.ttf").fontName, 50, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		WaterMark.antialiasing = ClientPrefs.data.antialiasing;
-		add(WaterMark);
-        
-        EventText= new FlxText(isTransIn ? 50 : -1230, 720 - 50 - 50, 0, 'LOADING . . . . . . ', 50);
-		EventText.scrollFactor.set();
-		EventText.setFormat(Assets.getFont("assets/fonts/loadText.ttf").fontName, 50, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		EventText.antialiasing = ClientPrefs.data.antialiasing;
-		add(EventText);
-		
 		if(!isTransIn) {
-		    try{
-			FlxG.sound.play(Paths.sound('loading_close_move'),ClientPrefs.data.CustomFadeSound);
-			}
-			if (!ClientPrefs.data.CustomFadeText) {
-			    EventText.text = '';
-			    WaterMark.text = '';
-			}
-			loadLeftTween = FlxTween.tween(loadLeft, {x: 0}, duration, {
+		    try{	
+			loadLeftTween = FlxTween.tween(loadLeft, {x: 10}, duration, {
 				onComplete: function(twn:FlxTween) {
 					if(finishCallback != null) {
 						finishCallback();
 					}
 				},
 			ease: FlxEase.expoInOut});
-			
-			loadRightTween = FlxTween.tween(loadRight, {x: 0}, duration, {
-				onComplete: function(twn:FlxTween) {
-					if(finishCallback != null) {
-						finishCallback();
-					}
-				},
-			ease: FlxEase.expoInOut});
-			
-			loadTextTween = FlxTween.tween(WaterMark, {x: 50}, duration, {
-				onComplete: function(twn:FlxTween) {
-					if(finishCallback != null) {
-						finishCallback();
-					}
-				},
-			ease: FlxEase.expoInOut});
-			
-			EventTextTween = FlxTween.tween(EventText, {x: 50}, duration, {
-				onComplete: function(twn:FlxTween) {
-					if(finishCallback != null) {
-						finishCallback();
-					}
-				},
-			ease: FlxEase.expoInOut});
-			
+					
 		} else {
-		    try{
-			    FlxG.sound.play(Paths.sound('loading_open_move'),ClientPrefs.data.CustomFadeSound);
-			}
-			EventText.text = 'COMPLETED !';
-			if (!ClientPrefs.data.CustomFadeText) {
-			    EventText.text = '';
-			    WaterMark.text = '';
-			}
+		    try{	    
 			loadLeftTween = FlxTween.tween(loadLeft, {x: -1280}, duration, {
-				onComplete: function(twn:FlxTween) {
-					close();
-				},
-			ease: FlxEase.expoInOut});
-			
-			loadRightTween = FlxTween.tween(loadRight, {x: 1280}, duration, {
-				onComplete: function(twn:FlxTween) {
-					close();
-				},
-			ease: FlxEase.expoInOut});
-			
-			loadTextTween = FlxTween.tween(WaterMark, {x: -1230}, duration, {
-				onComplete: function(twn:FlxTween) {
-					close();
-				},
-			ease: FlxEase.expoInOut});
-			
-			EventTextTween = FlxTween.tween(EventText, {x: -1230}, duration, {
 				onComplete: function(twn:FlxTween) {
 					close();
 				},
@@ -152,76 +72,10 @@ class CustomFadeTransition extends FlxSubState {
 		add(loadAlpha);
 		loadAlpha.setGraphicSize(FlxG.width, FlxG.height);
 		loadAlpha.updateHitbox();
-		
-		WaterMark = new FlxText( 50, 720 - 50 - 50 * 2, 0, 'NF ENGINE V' + MainMenuState.novaFlareEngineVersion, 50);
-		WaterMark.scrollFactor.set();
-		WaterMark.setFormat(Assets.getFont("assets/fonts/loadText.ttf").fontName, 50, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		WaterMark.antialiasing = ClientPrefs.data.antialiasing;
-		add(WaterMark);
-        
-        EventText= new FlxText( 50, 720 - 50 - 50, 0, 'LOADING . . . . . . ', 50);
-		EventText.scrollFactor.set();
-		EventText.setFormat(Assets.getFont("assets/fonts/loadText.ttf").fontName, 50, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		EventText.antialiasing = ClientPrefs.data.antialiasing;
-		add(EventText);
-		
-		if(!isTransIn) {
-		    try{
-			    FlxG.sound.play(Paths.sound('loading_close_alpha'),ClientPrefs.data.CustomFadeSound);
-			}
-			if (!ClientPrefs.data.CustomFadeText) {
-			    EventText.text = '';
-			    WaterMark.text = '';
-			}
-			WaterMark.alpha = 0;
-			EventText.alpha = 0;
-			loadAlpha.alpha = 0;
-			loadAlphaTween = FlxTween.tween(loadAlpha, {alpha: 1}, duration, {
-				onComplete: function(twn:FlxTween) {
-					if(finishCallback != null) {
-						finishCallback();
-					}
-				},
-			ease: FlxEase.sineInOut});
-			
-			loadTextTween = FlxTween.tween(WaterMark, {alpha: 1}, duration, {
-				onComplete: function(twn:FlxTween) {
-					if(finishCallback != null) {
-						finishCallback();
-					}
-				},
-			ease: FlxEase.sineInOut});
-			
-			EventTextTween = FlxTween.tween(EventText, {alpha: 1}, duration, {
-				onComplete: function(twn:FlxTween) {
-					if(finishCallback != null) {
-						finishCallback();
-					}
-				},
-			ease: FlxEase.sineInOut});
-			
+
 		} else {
-		    try{
-			    FlxG.sound.play(Paths.sound('loading_open_alpha'),ClientPrefs.data.CustomFadeSound);
-            }
-			EventText.text = 'COMPLETED !';
-			if (!ClientPrefs.data.CustomFadeText) {
-			    EventText.text = '';
-			    WaterMark.text = '';
-			}
+		    try{	    
 			loadAlphaTween = FlxTween.tween(loadAlpha, {alpha: 0}, duration, {
-				onComplete: function(twn:FlxTween) {
-					close();
-				},
-			ease: FlxEase.sineInOut});
-			
-			loadTextTween = FlxTween.tween(WaterMark, {alpha: 0}, duration, {
-				onComplete: function(twn:FlxTween) {
-					close();
-				},
-			ease: FlxEase.sineInOut});
-			
-			EventTextTween = FlxTween.tween(EventText, {alpha: 0}, duration, {
 				onComplete: function(twn:FlxTween) {
 					close();
 				},
