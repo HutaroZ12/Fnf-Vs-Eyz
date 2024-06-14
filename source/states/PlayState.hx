9319d272b7adf872a85a228b8598efa1106e33ea
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-06-14 15:00:06 -0300

    HMMMMMM

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -557,7 +557,6 @@ class PlayState extends MusicBeatState
           	blackBars.visible = true;
         	blackBars.scrollFactor.set();    		
          	add(blackBars);
-        	blackBars.camera = camHUD;
 
 		timeBar = new Bar(0, timeTxt.y + (timeTxt.height / 4), 'timeBar', function() return songPercent, 0, 1);
 		timeBar.scrollFactor.set();
