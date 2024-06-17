98966d8dfc302ed57daf8af13e0a9a51a273237e
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-06-17 20:52:40 -0300

    Update ZeroWeek1

--- source/states/stages/ZeroWeek1.hx
+++ source/states/stages/ZeroWeek1.hx
@@ -6,13 +6,13 @@ class Template extends BaseStage
 {
 	override function create()
 	{
-		layer1 = new BGSprite('Stages/zero week 1/layer1', -500, -300);
+	layer1 = new BGSprite('Stages/zero week 1/layer1', -500, -300);
         layer1.scrollFactor.set(1, 1);
         layer1.scale.x = 1.1;
         layer1.scale.y = 1.1;
         add(layer1);
 
-		layer2 = new BGSprite('Stages/zero week 1/layer2', -500, -300);
+	layer2 = new BGSprite('Stages/zero week 1/layer2', -500, -300);
         layer2.scrollFactor.set(1, 1);
         layer2.scale.x = 1.1;
         layer2.scale.y = 1.1;
@@ -36,7 +36,7 @@ class Template extends BaseStage
         layer5.scale.y = 1.1;
         add(layer5);
         
-        layer5 = new BGSprite('menuExtend/PlayState/blackBars', -500, -300);
+        blacBars = new BGSprite('menuExtend/PlayState/blackBars', -500, -300);
         blackBars.scrollFactor.set(1, 1);
         blackBars.scale.x = 1;
         blackBars.scale.y = 1;
