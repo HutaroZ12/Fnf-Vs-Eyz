7371dad02c8deb888efd09035c873429fc1ae9e1
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-06-18 22:32:54 -0300

    Update ChartingState.hx

--- source/states/editors/ChartingState.hx
+++ source/states/editors/ChartingState.hx
@@ -986,12 +986,10 @@ class ChartingState extends MusicBeatState
 		var eventPushedMap:Map<String, Bool> = new Map<String, Bool>();
 		var directories:Array<String> = [];
 
-		#if MODS_ALLOWED
 		directories.push(Paths.mods('custom_events/'));
 		directories.push(Paths.mods(Mods.currentModDirectory + '/custom_events/'));
 		for(mod in Mods.getGlobalMods())
 			directories.push(Paths.mods(mod + '/custom_events/'));
-		#end
 
 		for (i in 0...directories.length) {
 			var directory:String =  directories[i];
