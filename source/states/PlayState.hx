f5356fe44aab8108341ef65bb652d798b7bfa265
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-06-19 18:19:31 -0300

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2217,6 +2217,8 @@ class PlayState extends MusicBeatState
 		var iconOffset:Int = 26;
 		iconP1.x = healthBar.barCenter + (150 * iconP1.scale.x - 150) / 2 - iconOffset;
 		iconP2.x = healthBar.barCenter - (150 * iconP2.scale.x) / 2 - iconOffset * 2;
+		iconP1.animation.curAnim.curFrame = (healthBar.percent > 80) ? 2 : 0;
+		iconP2.animation.curAnim.curFrame = (healthBar.percent < 20) ? 2 : 0;
 	}
 
 	var iconsAnimations:Bool = true;
