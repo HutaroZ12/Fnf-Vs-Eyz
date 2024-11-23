403e76dac6d0d67c8107aca777715e5975c3c3c8
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2024-11-23 20:18:17 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -133,8 +133,7 @@ class MainMenuState extends MusicBeatState
 		menuItems.add(menuItem);
 		return menuItem;
 
-
-		switch (option)
+		switch (optionShit)
 		{
 		  case 'story_mode':
 		    FlxTween.tween(menuItem, {x:650}, 2.4, {ease: FlxEase.expoInOut});
